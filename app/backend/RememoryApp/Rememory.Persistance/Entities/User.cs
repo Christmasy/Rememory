a1354f5855238bfe902a249ea816cca21ea0c98d
NovoselovKirill <K.S.Novoselov@urfu.me>
2023-04-12 14:48:49 +0500

    add class TelegramInfo moved everything related to telegram from User to TelegramInfo

--- app/backend/RememoryApp/Rememory.Persistance/Entities/User.cs
+++ app/backend/RememoryApp/Rememory.Persistance/Entities/User.cs
@@ -1,7 +1,4 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
+﻿using MongoDB.Bson.Serialization.Attributes;
 using Rememory.Persistance.Models;
 
 namespace Rememory.Persistance.Entities;
@@ -10,9 +7,5 @@ public class User : IDatabaseEntity
 {
     [BsonId]
     public Guid Id { get; set; }
-    public Guid TelegramId { get; set; }
-    
-    [JsonConverter(typeof(StringEnumConverter))]
-    [BsonRepresentation(BsonType.String)]
-    public TelegramState TelegramState { get; set; }
+    public TelegramInfo? TelegramInfo { get; set; }
 }
---
+++ app/backend/RememoryApp/Rememory.Persistance/Models/TelegramInfo.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Rememory.Persistance.Models;
+
+public class TelegramInfo
+{
+    public string? TelegramId { get; set; }
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    [BsonRepresentation(BsonType.String)]
+    public TelegramState TelegramState { get; set; }
+    
+    /// <summary>
+    /// A GUID representing the ID of the object (journey, day, etc.)
+    /// the user is currently interacting with, if applicable.
+    /// </summary>
+    public Guid ObjectId { get; set; }
+}
